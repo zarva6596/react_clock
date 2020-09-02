45d7f021be38a4dde24398bf74d920561e989bc7
Zarva <zarva6596@gmail.com>
2020-09-03 02:09:25 +0300

    Updates

---
+++ precache-manifest.5bed730d9629f82e3c694af93528415f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22092ab7f9a15f27c6e777e2b3119ce2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "23940b1b7ea378fe8427",
+    "url": "./static/css/main.8ffd47c3.chunk.css"
+  },
+  {
+    "revision": "8daccb04a7f88300915b",
+    "url": "./static/js/2.754e2893.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.754e2893.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23940b1b7ea378fe8427",
+    "url": "./static/js/main.b7855161.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
